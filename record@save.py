8377422550013f644d34a2ad0c1c6c7b1dd4b701
Mah Tian Hoe <sheeyao_mah93@hotmail.com>
2016-08-12 16:59:40 +0800

    Update on save video
    Reconfigure saved video, image format to mjpg

--- record@save.py
+++ record@save.py
@@ -1,25 +1,27 @@
 import numpy as np
 import cv2
 
-# Capture video from camera
 cap = cv2.VideoCapture(0)
 
-# Capture video from file
-#cap = cv2.VideoCapture('BlueUmbrella.webm')
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
 
 while(cap.isOpened()):
-    # Capture frame-by-frame
     ret, frame = cap.read()
+    if ret == True:
+        #frame = cv2.flip(frame, 0)
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # write the flipped frame
+        out.write(frame)
 
-    # Display the resulting frame
-    cv2.imshow('frame', gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
         break
 
-# When everything done, release the capture
+# Release everything if job is finished
 cap.release()
-
+out.release()
 cv2.destroyAllWindows()
