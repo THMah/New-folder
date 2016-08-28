fc174315d75996b9792cab32f55b3c9506125649
Mah Tian Hoe <sheeyao_mah93@hotmail.com>
2016-08-28 22:03:16 +0800

    img to gray to bw
    commented out filder
    convert img to bw (testing which method is more usable)

--- test.py
+++ test.py
@@ -39,17 +39,26 @@ cv2.imshow("Show", crop_img)
 busLine = crop_img[y: y + h, x: x + w]
 
 #zooming in the image, better viewing
-resizeBusLine = cv2.resize(busLine, None, fx=3, fy=3,
+resizeBusLine = cv2.resize(busLine, None, fx=5, fy=5,
      interpolation=cv2.INTER_LINEAR)
 
 #2D filter
-kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-resizeBusLine = cv2.filter2D(resizeBusLine, -1, kernel)
+#kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+#resizeBusLine = cv2.filter2D(resizeBusLine, -1, kernel)
 
-#kernel = np.ones((1, 1), np.int8)
-#opening = cv2.morphologyEx(resizeBusLine, cv2.MORPH_OPEN, kernel)
+
+kernel = np.ones((1, 1), np.int8)
+opening = cv2.morphologyEx(resizeBusLine, cv2.MORPH_OPEN, kernel)
 #erosion = cv2.erode(resizeBusLine,kernel,iterations = 3)
 #dilation = cv2.dilate(busLine, kernel, iterations=1)
-cv2.imshow("Detected Bus Line", resizeBusLine)
+grayimg = cv2.cvtColor(opening, cv2.COLOR_BGR2GRAY)
+#thresh = 127
+#im_bw = cv2.threshold(grayimg, thresh, 255, cv2.THRESH_BINARY)[1]
+im_bw = cv2.adaptiveThreshold(grayimg,255,cv2.THRESH_BINARY,\
+cv2.THRESH_BINARY,11,2)
+
+#erosion = cv2.erode(resizeBusLine,kernel,iterations = 3)
+#dilation = cv2.dilate(busLine, kernel, iterations=1)
+cv2.imshow("Detected Bus Line", im_bw)
 cv2.waitKey()
 cv2.destroyAllWindows()
