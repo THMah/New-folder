6c291bf74d284dc294a3ef9e727c9d7453c022a0
THMah <sheeyao_mah93@hotmail.com>
2016-11-11 14:02:09 +0800

    loop all images in caffe

--- caffe_test.py
+++ caffe_test.py
@@ -2,24 +2,44 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import caffe
+import glob
+import os
+
 
 print 'load files'
-MODEL_FILE = "/home/th/Downloads/alexnet_models_new/deploy.prototxt"
-PRETRAINED = "/home/th/Downloads/alexnet_models_new/snapshot_iter_300.caffemodel"
-IMAGE_FILE = "/home/th/Desktop/bus_dataset_2/770/770_2_150.jpg"
-print 'set mode'
-caffe.set_mode_cpu()
-print 'load pretrained'
-net = caffe.Classifier(MODEL_FILE, PRETRAINED,
-                       channel_swap=(2, 1, 0),
-                        mean=np.load("/home/th/Downloads/alexnet_models_new/a.npy"),
-                       raw_scale=255,
-                       image_dims=(256, 256))
-input_image = caffe.io.load_image(IMAGE_FILE)
-plt.imshow(input_image)
+MODEL_FILE = "/home/th/Downloads/alexnet_models_3bus_100echo/deploy.prototxt"
+PRETRAINED = "/home/th/Downloads/alexnet_models_3bus_100echo/snapshot_iter_1800.caffemodel"
+IMAGE_FILE = "/home/th/Desktop/bus_dataset/SJ01/bus_771_275.jpg"
+VIDEO_FILE = "/home/th/Desktop/bus video oct/Editted/bus_771.mp4"
+VIDEO_FILE1 = glob.glob("/home/th/Desktop/bus video oct/Editted/bus_771.mp4")
+IMAGE_FILE1 = "/home/th/Desktop/bus_771_test_caffe"
 
-prediction = net.predict([input_image])  # predict takes any number of images, and formats them for the Caffe net automatically
-print 'prediction shape:', prediction[0].shape
-plt.plot(prediction[0])
-print 'predicted class:', prediction[0].argmax()
-plt.show()
+for file in os.listdir(IMAGE_FILE1):
+    if file.endswith(".jpg"):
+        print (IMAGE_FILE1 + '/' + file)
+
+        print 'set mode'
+        #use GPU to do calculation
+        caffe.set_mode_gpu()
+
+        #use CPU to do calculation
+        #caffe.set_mode_cpu()
+
+        print 'load pretrained'
+        net = caffe.Classifier(MODEL_FILE, PRETRAINED,
+                                #RGB to BGR
+                               channel_swap=(2, 1, 0),
+                               raw_scale=255,
+                               image_dims=(227, 227))
+        print 'input image'
+        input_image = caffe.io.load_image(IMAGE_FILE1 + '/' + file)
+        print 'show image'
+        plt.imshow(input_image)
+
+        #predict takes any number of images, and formats them for the Caffe net automatically
+        prediction = net.predict([input_image])
+        #print 'prediction shape:', prediction[0].shape
+        plt.plot(prediction[0])
+        output = ["770", "771", "SJ01", "No bus"]
+        print 'predicted class:', output[prediction[0].argmax()]
+        plt.show()
