2091a1e34959502b0df3da98b4536dc8b5166079
THMah <sheeyao_mah93@hotmail.com>
2016-11-12 15:26:03 +0800

    Add files via upload
    -combined record, save and classification of images

--- caffe_test.py
+++ caffe_test.py
@@ -1,22 +1,40 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+import datetime
 import caffe
-import glob
 import os
+
 
 
 print 'load files'
 MODEL_FILE = "/home/th/Downloads/alexnet_models_3bus_100echo/deploy.prototxt"
 PRETRAINED = "/home/th/Downloads/alexnet_models_3bus_100echo/snapshot_iter_1800.caffemodel"
-IMAGE_FILE = "/home/th/Desktop/bus_dataset/SJ01/bus_771_275.jpg"
-VIDEO_FILE = "/home/th/Desktop/bus video oct/Editted/bus_771.mp4"
-VIDEO_FILE1 = glob.glob("/home/th/Desktop/bus video oct/Editted/bus_771.mp4")
-IMAGE_FILE1 = "/home/th/Desktop/bus_771_test_caffe"
+IMAGE_FOLDER = "/home/th/Desktop/bus_771_test_caffe"
+LIVE_VIDEO_FRAME = '/home/th/Desktop/FYP_latest/New-folder/Live_video_frame'
 
-for file in os.listdir(IMAGE_FILE1):
+print 'capture video'
+#use 0 to capture live video / VideoPath to play local video
+cap = cv2.VideoCapture("/home/th/Desktop/1s_video/bus1.mp4")
+#frame rate
+frameRate = cap.get(5)
+while(cap.isOpened()):
+#current frame number
+    frameId = cap.get(1)
+    ret, frame = cap.read()
+    if (ret != True):
+        break
+    if (frameId % math.floor(frameRate) == 0):
+        resizeImage = cv2.resize(frame, (256, 256))
+        now = format(datetime.datetime.now())
+        filename = LIVE_VIDEO_FRAME + "/" + str(now) + ".jpg"
+        cv2.imwrite(filename, resizeImage)
+cap.release()
+#end capture video and save as image
+
+for file in os.listdir(LIVE_VIDEO_FRAME):
     if file.endswith(".jpg"):
-        print (IMAGE_FILE1 + '/' + file)
 
         print 'set mode'
         #use GPU to do calculation
@@ -32,14 +50,15 @@ for file in os.listdir(IMAGE_FILE1):
                                raw_scale=255,
                                image_dims=(227, 227))
         print 'input image'
-        input_image = caffe.io.load_image(IMAGE_FILE1 + '/' + file)
+        input_image = caffe.io.load_image(LIVE_VIDEO_FRAME + '/' + file)
         print 'show image'
         plt.imshow(input_image)
-
         #predict takes any number of images, and formats them for the Caffe net automatically
         prediction = net.predict([input_image])
         #print 'prediction shape:', prediction[0].shape
         plt.plot(prediction[0])
         output = ["770", "771", "SJ01", "No bus"]
         print 'predicted class:', output[prediction[0].argmax()]
+        str1 = 'bus' + str(output[prediction[0].argmax()])
+        plt.text(2, 1, str1, fontsize=15)
         plt.show()
